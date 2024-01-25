1f869a10b84291e1477e4f84a9ea522fa371d398
Kofi Boakye <59366644+reyzuki@users.noreply.github.com>
2024-01-25 10:58:30 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Dashboard from './pages/Component';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
